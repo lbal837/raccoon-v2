dcbfefc73e8412dc6e71506450a8a6fdb6bacc31
lbal837 <91184091+lbal837@users.noreply.github.com>
2023-09-08 11:29:19 +1200

    Asks user how long they want to do an activity for

--- reminder.py
+++ reminder.py
@@ -1,53 +1,90 @@
 from tkinter import *
 import time
+from tkinter import ttk
 
 root = Tk()
 root.title('Reminder Raccoon')
 root.geometry("400x100")
 
 
-def close_popup():
-    time.sleep(3)  # waits 3 seconds, irl should b 15 minutes
+def close_popup(wait):
+    print("I am waiting " + str(wait) + " minutes")
+    time.sleep(wait)  # waits "wait" minutes
     root.deiconify()
     run_app()
 
 
 class OpenMessage:
     def __init__(self, master):
+        self.activity_button = None
+        self.activity = None
+        self.activity_entry = None
+        self.webpage = IntVar(value=0)
+        self.close_webpage = None
+        self.time_entry = None
         self.master = master
-        self.mainFrame = Frame(self.master)
-        self.leaveFrame = Frame(self.master)
-        self.mainFrame.pack()
-        self.distracted = Label(self.mainFrame, text="Distracted?")  # see if you can label without name
-        self.stay_here = Label(self.mainFrame,
+        self.main_frame = Frame(self.master)
+        self.leave_frame = Frame(self.master)
+        self.main_frame.pack()
+        self.distracted = Label(self.main_frame, text="Distracted?")  # see if you can label without name
+        self.stay_here = Label(self.main_frame,
                                text="Do you want to stay on this webpage?")  # see if you can label without name
-        self.button1 = Button(self.mainFrame, text="Yes", fg="red", command=self.stay)
-        self.button2 = Button(self.mainFrame, text="No", fg="blue", command=self.leave)
+        self.button1 = Button(self.main_frame, text="Yes", fg="red", command=lambda: self.hide_main_frame(self.stay()))
+        self.button2 = Button(self.main_frame, text="No", fg="blue", command=lambda: self.hide_main_frame(self.leave()))
         self.distracted.grid(row=0, columnspan=2)
         self.stay_here.grid(row=1, columnspan=2)
         self.button1.grid(row=2)
         self.button2.grid(row=2, column=1)
 
     def leave(self):
-        self.hide_main_frame()
-        self.leaveFrame = Frame(self.master)
-        self.leaveFrame.pack()
-        Label(self.leaveFrame, text="Close this page and get to work!!").pack()
-        self.goodbye()
+        t = "Close the webpage you are on"
+        self.close_webpage = Checkbutton(self.leave_frame, variable=self.webpage, text=t, command=lambda: self.close())
+        self.close_webpage.grid(row=0, columnspan=2)
 
-    def stay(self):
-        self.hide_main_frame()
-        self.leaveFrame.pack()
-        Label(self.leaveFrame, text="Great, keep it up!").pack()
-        self.goodbye()
+    def close(self):
+        self.close_webpage.config(state=DISABLED)
+        self.activity = Label(self.leave_frame, text="What would you like to do now?")  # make text space bigger
+        self.activity_entry = Entry(self.leave_frame)
+        self.activity.grid(row=1, column=0)
+        self.activity_entry.grid(row=1, column=1)
+        self.activity_button = Button(self.leave_frame, text="Enter", fg="red",
+                                      command=lambda: self.hide_leave_frame(
+                                          self.stay("how long would you like to do this for?(minutes)")))
+        self.activity_button.grid(row=2, columnspan=2)
 
-    def goodbye(self):
-        self.leaveFrame.after(1500, lambda: self.master.withdraw())
-        self.leaveFrame.after(1500, lambda: self.leaveFrame.pack_forget())
-        self.leaveFrame.after(1500, lambda: close_popup())
+    def stay(self, t="Great, how much longer would you like to stay on this page?(minutes)"):
+        Label(self.leave_frame, text=t).grid(row=0,
+                                             columnspan=2)
+        self.time_entry = ttk.Combobox(self.leave_frame,
+                                       state="readonly",
+                                       values=[5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
+                                       )
+        task_button = Button(self.leave_frame, text="Enter", fg="red", command=lambda: self.checker())
+        self.time_entry.grid(row=1, column=0)
+        task_button.grid(row=1, column=1)
 
-    def hide_main_frame(self):
-        self.mainFrame.pack_forget()
+    def checker(self):
+        if self.time_entry.get() == '':
+            Label(self.leave_frame, text="no time entered! try again").pack()
+        else:
+            self.goodbye(1, int(self.time_entry.get()))
+
+    def goodbye(self, buffer=1500, wait=15):
+        self.leave_frame.after(buffer, lambda: self.master.withdraw())
+        self.leave_frame.after(buffer, lambda: self.leave_frame.pack_forget())
+        self.leave_frame.after(buffer, lambda: close_popup(wait))
+
+    def hide_leave_frame(self, function):
+        self.activity.grid_forget()
+        self.activity_entry.grid_forget()
+        self.activity_button.grid_forget()
+        self.close_webpage.grid_forget()
+        return function
+
+    def hide_main_frame(self, function):
+        self.main_frame.pack_forget()
+        self.leave_frame.pack()
+        return function
 
 
 def run_app():
