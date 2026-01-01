2a972213caf290aae9de8a7a96a8dae504242f10
lbal837 <91184091+lbal837@users.noreply.github.com>
2026-01-01 13:05:40 +1300

    update so you have to enter an activity and remove all csv stuff

--- reminder.py
+++ reminder.py
@@ -1,13 +1,11 @@
 from tkinter import *
 from tkinter import ttk
-import csv
 from datetime import datetime
 
 root = Tk()
 root.title('Reminder Raccoon')
 minutes_working = 0
 total_minutes_working = 0
-csv_file_path = datetime.today().strftime('%d-%m-%Y-%H-%M-%S') + ".csv"
 
 
 def close_popup(mins):
@@ -42,8 +40,7 @@ class OpenMessage:
         self.feel_error = None
         self.activity_for_timer = None
         self.timer_label = None
-        self.button2 = None
-        self.button1 = None
+
         self.stay_here = None
         self.distracted_label = None
         self.feel_button = None
@@ -53,13 +50,10 @@ class OpenMessage:
         self.activity_button = None
         self.activity = None
         self.activity_entry = None
-        self.webpage = IntVar(value=0)
-        self.close_webpage = None
         self.time_entry = None
         self.break_true = False
         self.master = master
         self.minutes_working = minutes_working
-        self.csv_file_path = csv_file_path
         self.main_frame = Frame(self.master)
         self.leave_frame = Frame(self.master)
         self.distracted_frame = Frame(self.master)
@@ -68,16 +62,6 @@ class OpenMessage:
         self.minutes = 00
         self.secs = 00
         self.feelings()
-
-    def end_session(self):
-        pass
-
-    def write_to_file(self):
-        time = datetime.today().strftime('%I:%M%p')
-        new = [time, self.feel_entry.get(), self.activity_for_timer, self.minutes]
-        with open(self.csv_file_path, mode='a', newline="") as fd:
-            appender = csv.writer(fd)
-            appender.writerow(new)
 
     def feelings(self):
         self.main_frame.pack()
@@ -92,19 +76,17 @@ class OpenMessage:
     def distracted(self):
         feel_text = "you feel " + self.feel_entry.get()
         self.distracted_label = Label(self.distracted_frame, text=feel_text)  # see if you can label without name
-        self.stay_here = Label(self.distracted_frame,
-                               text="Do you want to stay on this webpage?")  # see if you can label without name
-        self.button1 = Button(self.distracted_frame, text="Yes", fg="red",
-                              command=lambda: self.hide_distracted_frame(self.stay()))
-        self.button2 = Button(self.distracted_frame, text="No", fg="blue",
-                              command=lambda: self.hide_distracted_frame(self.leave()))
+        self.activity = Label(self.distracted_frame, text="What would you like to do now?")  # see if you can label without name
+        self.activity_entry = Entry(self.distracted_frame)
+        self.activity_button = Button(self.distracted_frame, text="Enter", fg="red",
+                                      command=lambda: self.activity_checker())
         self.break_button = Button(self.distracted_frame, text="I need a break", fg="green",
                                    command=lambda: self.hide_break_frame(self.distracted_frame))
         self.distracted_label.grid(row=0, columnspan=2)
-        self.stay_here.grid(row=1, columnspan=2)
-        self.button1.grid(row=2)
-        self.button2.grid(row=2, column=1)
-        self.break_button.grid(row=3, column=0)
+        self.activity.grid(row=1, columnspan=2)
+        self.activity_entry.grid(row=2, column=0)
+        self.activity_button.grid(row=2, column=1)
+        self.break_button.grid(row=4, column=0)
 
     def take_a_break(self, break_text):
         self.break_frame.pack()
@@ -137,22 +119,7 @@ class OpenMessage:
         self.something_else_button.grid(row=6, column=2)
         self.continue_working_button.grid(row=7, column=3)
 
-    def leave(self):
-        t = "Close the webpage you are on"
-        self.close_webpage = Checkbutton(self.leave_frame, variable=self.webpage, text=t, command=lambda: self.close())
-        self.close_webpage.grid(row=0, columnspan=2)
-
-    def close(self):
-        self.close_webpage.config(state=DISABLED)
-        self.activity = Label(self.leave_frame, text="What would you like to do now?")  # make text space bigger
-        self.activity_entry = Entry(self.leave_frame)
-        self.activity.grid(row=1, column=0)
-        self.activity_entry.grid(row=1, column=1)
-        self.activity_button = Button(self.leave_frame, text="Enter", fg="red",
-                                      command=lambda: self.activity_checker())
-        self.activity_button.grid(row=2, columnspan=2)
-
-    def stay(self, t="Great, how much longer would you like to stay on this page?(minutes)"):
+    def stay(self, t):
         Label(self.leave_frame, text=t).grid(row=0,
                                              columnspan=2)
         self.time_entry = ttk.Combobox(self.leave_frame,
@@ -179,38 +146,30 @@ class OpenMessage:
 
     def activity_checker(self):
         if self.activity_entry.get() == '' and self.activity_error is None:
-            self.activity_error = Label(self.leave_frame, text="no activity entered! try again")
+            self.activity_error = Label(self.distracted_frame, text="no activity entered! try again")
             self.activity_error.grid(row=3)
         elif self.activity_entry.get() != '':
             t = "how long would you like to " + self.activity_entry.get() + " for?(minutes)"
-            self.hide_leave_frame(self.stay(t))
+            self.hide_distracted_frame(self.stay(t))
 
     def goodbye(self, buffer=1500):
-        # self.leave_frame.after(buffer, lambda: self.master.withdraw())
+
         self.leave_frame.after(buffer, lambda: self.leave_frame.pack_forget())
-        # if self.activity_entry is not None:
-        # self.leave_frame.after(buffer, lambda: close_popup(wait, self.activity_entry.get()))
-        # else:
-        # self.leave_frame.after(buffer, lambda: close_popup(wait))
+
         self.leave_frame.after(buffer, lambda: self.timer_display())
 
     def timer_display(self):
 
-        # self.master.geometry("+600+250")
-        # root.geometry("50x50")
-
         self.timer_frame.pack()
-        if self.activity_entry is None:
-            if self.break_true:
-                self.activity_for_timer = "take a break"
-            else:
-                self.activity_for_timer = "stay on this webpage"
+
+        if self.break_true:
+            self.activity_for_timer = "take a break"
+
         else:
             self.activity_for_timer = self.activity_entry.get()
         self.timer_label = Label(
             self.timer_frame, text=f"I am waiting {self.minutes}m: {self.secs}s for you to {self.activity_for_timer}")
         self.timer_label.pack()
-        self.write_to_file()
         self.countdown()
 
     def countdown(self):
@@ -232,14 +191,7 @@ class OpenMessage:
         else:
             self.timer_frame.after(1000, lambda: close_popup(count_init / 60))
 
-    def hide_leave_frame(self, function):
-        self.activity.grid_forget()
-        self.activity_entry.grid_forget()
-        self.activity_button.grid_forget()
-        self.close_webpage.grid_forget()
-        if self.activity_error is not None:
-            self.activity_error.grid_forget()
-        return function
+
 
     def hide_distracted_frame(self, function):
         self.distracted_frame.pack_forget()
@@ -252,6 +204,7 @@ class OpenMessage:
         if self.minutes_working >= 60:
             return self.take_a_break("You have been on your computer for 1 hour or more!")
         else:
+            root.geometry("400x120")
             self.distracted_frame.pack()
             return self.distracted()
 
@@ -276,10 +229,4 @@ def run_app():
     root.mainloop()
 
 
-data = ['Time', 'Feeling', 'Activity', 'Minutes']
-with open(csv_file_path, mode='w', newline='') as file:
-    # Create a csv.writer object
-    writer = csv.writer(file)
-    # Write data to the CSV file
-    writer.writerow(data)
 run_app()
